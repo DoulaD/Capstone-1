0f84601ed92ce837b1262d07426999d73f04d0e6
DoulaD <Douladoumbia@gmail.com>
2025-04-28 10:32:26 -0400

    Getter/Setter

--- AccountingLedger/src/main/java/com/pluralsight/financetracker/Transaction.java
+++ AccountingLedger/src/main/java/com/pluralsight/financetracker/Transaction.java
@@ -14,4 +14,36 @@ public class Transaction {
         this.vendor = vendor;
         this.amount = amount;
     }
+
+    public LocalDateTime getPurchase() {
+        return purchase;
+    }
+
+    public void setPurchase(LocalDateTime purchase) {
+        this.purchase = purchase;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
 }
